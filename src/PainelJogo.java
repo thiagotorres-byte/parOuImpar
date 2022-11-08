1fc10a0aef8428f2a778a95e3142248425f9c4c2
Luiz Felipe <luizfelipe.learning@gmail.com>
2022-11-08 00:39:53 -0300

    Refatoração: painel de jogo para outro pacote

--- src/PainelJogo.java
+++
@@ -1,9 +0,0 @@
-public class PainelJogo {
-    public static void imprimir() {
-        System.out.println("Bem vindo ao jogo de ímpar ou par!");
-        System.out.println("---------------Menu---------------");
-        System.out.println("---- 1 - Jogador vs Jogador  -----");
-        System.out.println("---- 2 - Jogador vs CPU  ---------");
-        System.out.println("----------- 3 - Sair -------------");
-    }
-}
---
+++ src/br/anhembi/game/PainelJogo.java
@@ -0,0 +1,12 @@
+package br.anhembi.game;
+
+public class PainelJogo {
+    public static void imprimir() {
+        System.out.println(
+                "Bem vindo ao jogo de ímpar ou par!\\n" +
+                "---------------Menu---------------\\n" +
+                "---- 1 - Jogador vs Jogador  -----\\n" +
+                "---- 2 - Jogador vs CPU  ---------\\n" +
+                "----------- 3 - Sair -------------");
+    }
+}
